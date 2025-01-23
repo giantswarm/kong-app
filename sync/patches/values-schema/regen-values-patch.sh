7ac879bafacf758a7d5cc59c5b6b2a1b2fa6646a
Gerald Pape <gerald@giantswarm.io>
2025-01-23 14:55:39 +0100

    Remove unused file

--- sync/patches/values-schema/regen-values-patch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x
-
-git --no-pager diff -R --no-color --no-index helm/kong-app/values.yaml vendor/kong/charts/kong/values.yaml > sync/patches/values-schema/values.yaml.patch
-set -e
-sed -i 's|b/vendor/kong/charts/kong/values.yaml|b/helm/kong-app/values.yaml|' sync/patches/values-schema/values.yaml.patch
