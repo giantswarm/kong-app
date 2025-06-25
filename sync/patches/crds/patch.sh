f6a036d6afbb9b96a0cb4138542a0e9aaa479b27
Matías Charrière <matias@giantswarm.io>
2025-06-25 12:13:19 +0200

    Rename `helm/kong-app/crds` folder to `helm/kong-app/files` (#475)
    * rename crds folder as files
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * Update crds patch
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    * update changelog
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>
    
    ---------
    
    Signed-off-by: Matias Charriere <matias@giantswarm.io>

--- sync/patches/crds/patch.sh
+++ sync/patches/crds/patch.sh
@@ -9,10 +9,12 @@ script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ) ; readonly script_d
 
 cd "${repo_dir}"
 
+mkdir -p ./helm/kong-app/files
+
 versions="v3.0.2 v3.1.6 v3.2.4 v3.3.1 v3.4.3 v3.4.6"
 
 for version in $versions; do
-    crds_file="./helm/kong-app/crds/custom-resource-definitions-${version}.yaml"
+    crds_file="./helm/kong-app/files/custom-resource-definitions-${version}.yaml"
     # Fetch CRDs file
     echo "# generated using: kubectl kustomize 'github.com/kong/kubernetes-ingress-controller/config/crd?ref=${version}'" > "$crds_file"
     kubectl kustomize "github.com/kong/kubernetes-ingress-controller/config/crd?ref=${version}" >> "$crds_file"
@@ -23,5 +25,7 @@ readonly script_dir_rel=".${script_dir#"${repo_dir}"}"
 
 set -x
 git apply "${script_dir_rel}/custom-resource-definitions.yaml.patch"
+
+rm -r ./helm/kong-app/crds
 
 { set +x; } 2>/dev/null
